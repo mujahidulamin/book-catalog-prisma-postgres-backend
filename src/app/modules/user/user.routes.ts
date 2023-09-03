901c9323ef1a52b479e89b61535b02e164d3a848
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2023-09-03 13:36:13 +0600

    authGuard added for user

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,17 +1,12 @@
 import express from 'express';
 
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import { UserController } from './user.controller';
 
 const router = express.Router();
-router.get('/',  UserController.getAllUser);
-router.get('/:id', UserController.getSingleUser);
-router.patch(
-  '/:id',
-  UserController.updateUser,
-);
-router.delete(
-  '/:id',
-
-  UserController.deleteUser,
-);
+router.get('/', auth(ENUM_USER_ROLE.ADMIN), UserController.getAllUser);
+router.get('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.getSingleUser);
+router.patch('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.updateUser);
+router.delete('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.deleteUser);
 export const UserRoutes = router;
